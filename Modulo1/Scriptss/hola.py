1edfcf3acf2c80482bbbee557a0f0437d987ca76
Abait-25 <gamarra.ciomer@gmail.com>
2024-07-07 18:00:38 +0000

    primeraclase

---
+++ Modulo1/Scriptss/hola.py
@@ -0,0 +1,8 @@
+# comentario
+# los comentarios son para documentar el codigo
+# y no se ejecutan
+
+# control+s: guardar
+# "cd" para cambiar de directorio
+# "pwd" para ver el directorio
+print("hola mundo")
